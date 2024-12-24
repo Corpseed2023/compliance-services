dc1c99a5a42ff603f57ad32937bc4d0d9dcee82b
Corpseed2023 <kaushlendra.pratap@corpseed.com>
2024-12-24 14:59:17 +0530

    latest changes in compliance

--- src/main/java/com/lawzoom/complianceservice/dto/userDto/UserResponse.java
+++ src/main/java/com/lawzoom/complianceservice/dto/userDto/UserResponse.java
@@ -3,39 +3,18 @@ package com.lawzoom.complianceservice.dto.userDto;
 import lombok.*;
 
 import java.util.Date;
-import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@Builder
-public class UserResponse {
-
+public class UserResponseDto {
 	private Long id;
-
-	private String firstName;
-
-	private String lastName;
-
+	private String userName;
 	private String email;
-
 	private String mobile;
-
-	private String designation;
-
-	private String resourceType;
-
-	private Date createdAt;
-
-	private Date updatedAt;
-
 	private boolean isEnable;
-
 	private boolean isAssociated;
-
-	private Long userId;
-
-	private List<String> roles;
-
+	private Date createdAt;
+	private Date updatedAt;
 }
--- src/main/java/com/lawzoom/complianceservice/services/RoleService.java
+++ src/main/java/com/lawzoom/complianceservice/services/RoleService.java
@@ -1,18 +1,12 @@
 package com.lawzoom.complianceservice.services;
 
-
 import com.lawzoom.complianceservice.dto.roleDTO.RoleResponse;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface RoleService {
-    
-    ResponseEntity<Void> deleteRole(Long roleId);
+    public List<RoleResponse> fetchAllRoles();
 
-    ResponseEntity<String> createRole(String role);
-
-    List<RoleResponse> fetchAllRoles();
-
-    RoleResponse findEnabledAndNotDeletedRoleById(Long id);
+    public ResponseEntity<String> createRole(String roleName) ;
 }
